2e7e3caa7662a9addad5818ea3a5389ba347f2d7
VictorIdowu <idsonvictor@gmail.com>
2023-10-20 22:38:30 +0100

    Resposiveness for hero and secon section

--- src/pages/portfolio/CarefullyPicked.tsx
+++ src/pages/portfolio/CarefullyPicked.tsx
@@ -3,32 +3,38 @@ import React, { useState } from "react";
 const CarefullyPicked = () => {
   const [active, setActive] = useState(1);
 
+  // font
+
   return (
     <section className="w-full py-24 mt-14 bg-gray-100 flex-col justify-start ">
-      <aside className="max-w-[1240px] my-0 mx-auto items-center gap-12 flex flex-col px-10">
-        <h2 className=" text-center text-slate-800 text-6xl font-semibold font-redhat">
+      <aside className="max-w-[1240px] my-0 mx-auto items-center gap-12 flex flex-col px-3 sm:px-4 md:px-8 xl:px-0">
+        <h2 className=" text-center text-slate-800 text-3xl md:text-6xl font-semibold font-redhat">
           Carefully Picked Videos
         </h2>
         <div className="w-full flex-col justify-start items-end gap-14 flex">
-          <div className="w-full items-end gap-4 flex flex-col md:flex-row ">
+          <div className="w-full flex flex-col md:flex-row gap-4">
             <div className="w-full self-center justify-center  items-start gap-10 flex border-b border-gray-300">
               {["Businesses", "Individuals"].map((item, i) => (
                 <button
                   key={i + 1}
-                  className={`text-center lg:translate-x-1/2 pb-4  
-                      active:border-orange-500 active:border-b-4 active:text-orange-500 text-base font-bold font-['Noto Sans']`}
+                  className={`text-center md:translate-x-3/4 lg:translate-x-1/2 pb-4  
+                      ${
+                        i === 1
+                          ? "border-orange-500 border-b-4 text-orange-500"
+                          : ""
+                      } text-xl font-bold font-noto`}
                 >
                   {item}
                 </button>
               ))}
             </div>
 
-            <div className="w-full px-2 lg:w-fit py-2.5 bg-white rounded-lg border border-gray-300 justify-start items-center gap-2 flex">
+            <div className="w-full px-2 md:w-fit py-2.5 bg-white rounded-lg border border-gray-300 justify-start items-center gap-2 flex">
               <select className="grow shrink basis-0 justify-start items-center gap-2 flex pr-6">
                 {["Short Films", "Movies"].map((item, i) => (
                   <option
                     key={i + 1}
-                    className="text-slate-800 text-base font-normal font-noto leading-normal "
+                    className="text-slate-800 text-base  font-noto  "
                   >
                     {item}
                   </option>
@@ -36,9 +42,9 @@ const CarefullyPicked = () => {
               </select>
             </div>
           </div>
-          <div className="w-full flex-col justify-start items-start gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <ul className="w-full flex-col justify-start items-start gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {[1, 2, 3, 4, 5].map((item, i) => (
-              <div
+              <li
                 key={i + 1}
                 className="w-full h-96 relative rounded-lg overflow-hidden"
               >
@@ -47,7 +53,7 @@ const CarefullyPicked = () => {
                   src="https://img.freepik.com/premium-photo/falling-asleep-by-light-his-monitor-shot-exhausted-computer-programmer-sleeping-his-desk-night_590464-20430.jpg"
                   alt="tired"
                 />
-                <p className="bottom-6 left-10 right-5 absolute text-gray-100 text-2xl font-bold font-['Noto Sans'] leading-10">
+                <p className="bottom-6 left-10 right-5 absolute text-gray-100 text-2xl font-bold font-noto leading-10">
                   A Day in the life of a Software Developer
                 </p>
                 <div className="w-20 h-20 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 absolute bg-gray-50 rounded-3xl shadow justify-center items-center inline-flex">
@@ -55,25 +61,25 @@ const CarefullyPicked = () => {
                     <div className="w-6 h-6 relative"></div>
                   </div>
                 </div>
-              </div>
+              </li>
             ))}
-            <div className="w-full h-full relative rounded-lg overflow-hidden">
+            <li className="w-full h-96 relative rounded-lg overflow-hidden">
               <img
-                className="absolute blur-5 w-full h-full blur-lg brightness-50"
+                className="absolute z-10 w-full h-full blur-lg brightness-50"
                 src="https://img.freepik.com/premium-photo/falling-asleep-by-light-his-monitor-shot-exhausted-computer-programmer-sleeping-his-desk-night_590464-20430.jpg"
                 alt="tired"
               />
-              <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 self-stretch flex-col justify-start items-center gap-10 inline-flex">
-                <p className="w-80 text-center text-gray-100 text-2xl font-bold font-['Noto Sans'] leading-10">
+              <aside className="absolute z-30 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex flex-col items-center gap-10">
+                <p className="w-80 text-center text-gray-100 text-2xl font-bold font-noto leading-10">
                   This could be your project as a part of our amazing curation.
                 </p>
 
-                <button className="text-gray-900 text-base font-extrabold font-['Inter'] leading-normal px-5 py-3 bg-white rounded-lg shadow">
+                <button className="text-gray-900 text-base font-extrabold font-inter leading-normal px-5 py-3 bg-white rounded-lg shadow">
                   LET'S TALK!
                 </button>
-              </div>
-            </div>
-          </div>
+              </aside>
+            </li>
+          </ul>
         </div>
       </aside>
     </section>
