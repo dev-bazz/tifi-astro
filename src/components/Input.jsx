f178e267bacc182e915fac3433a33a0027fdf375
Dapo Adedire <adedireadedapo19@gmail.com>
2023-10-20 20:06:23 +0100

    feat: build "get in touch" page and footer

---
+++ src/components/Input.jsx
@@ -0,0 +1,32 @@
+
+export const Input = ({
+    label,
+    type,
+    name,
+    placeholder,
+    value,
+    onChange,
+    required,
+}) => {
+    return (
+        <div className="flex flex-col gap-[6px]">
+            <label
+            className="text-[#344054] text-[14px] font-medium"
+            >{label}</label>
+            <input
+                className="border border-[#98A2B3] rounded-[8px] px-[14px] py-[16px]
+                placeholder-[#98A2B3] text-base
+                focus:outline-none focus:border-[#344054]
+                bg-white
+                "
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                required={required}
+                
+            />
+        </div>
+    );
+};
