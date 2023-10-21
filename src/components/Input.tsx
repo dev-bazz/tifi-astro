3a18db42b61f225fe20fd08af07e432bf62ab5d3
Dapo Adedire <adedireadedapo19@gmail.com>
2023-10-21 08:20:27 +0100

    refactor(components) refactor jsx to tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,12 @@
+interface InputProps {
+    label: string;
+    type: string;
+    name: string;
+    placeholder: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean;
+}
 
 export const Input = ({
     label,
@@ -7,12 +16,12 @@ export const Input = ({
     value,
     onChange,
     required,
-}) => {
+}: InputProps) => {
     return (
         <div className="flex flex-col gap-[6px]">
-            <label
-            className="text-[#344054] text-[14px] font-medium"
-            >{label}</label>
+            <label className="text-[#344054] text-[14px] font-medium">
+                {label}
+            </label>
             <input
                 className="border border-[#98A2B3] rounded-[8px] px-[14px] py-[16px]
                 placeholder-[#98A2B3] text-base
@@ -25,7 +34,6 @@ export const Input = ({
                 value={value}
                 onChange={onChange}
                 required={required}
-                
             />
         </div>
     );
